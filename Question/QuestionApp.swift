a378bad8591a16c521351c425ac5eb3f79d71d9f
bhullarravia <bhullarravia@gmail.com>
2024-06-24 19:01:05 -0400

    Initial Commit

---
+++ Question/QuestionApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuestionApp.swift
+//  Question
+//
+//  Created by Ravia Bhullar on 6/24/24.
+//
+
+import SwiftUI
+
+@main
+struct QuestionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
